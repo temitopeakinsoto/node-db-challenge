40145a53f3ca53bfe1b0d27e70fd687c87fbe6dc
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-09 12:28:15 +0100

    Import Task and Resource routers
    In this commit, I imported the task-router.js and resource-router.js files which
    contain the GET / and POST / endpoints for both our resource and projects respectively

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const ProjectRouter = require('./projects/project-router.js');
+const TaskRouter = require('./projects/task-router.js');
+const ResourceRouter = require('./projects/resource-router.js');
+
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', ProjectRouter);
+server.use('/api/tasks', TaskRouter);
+server.use('/api/resources', ResourceRouter);
 
 module.exports = server;
