6aede5cdcebb73a20dac001a53c2caa1569ad8a8
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-10 00:07:59 +0100

    Add validateResourcePost middleware
    In this commit, I added a middleware called validateResourcePost(). This middleware
    checks to see if a resource post meets the minimum requirements for a resource post.
    The validateResourcePost middleware is then used to validate all requests to our
    GET/:id endpoint

--- projects/project-model.js
+++ projects/project-model.js
@@ -8,6 +8,7 @@ module.exports = {
     addTask,
     getTasksById,
     getResource,
+    getResourceById,
     addResource
   };
 
--- projects/resource-router.js
+++ projects/resource-router.js
@@ -4,6 +4,26 @@ const resources = require("./project-model.js");
 
 const router = express.Router();
 
+function validateResourceId(req, res, next){
+    const id = req.params.id;
+    resources.getResourceById(id)
+    .then(resource => {
+        if(resource){
+            req.resourceItem = resource;
+            next();
+        }else {
+            res.status(400).json({
+                message: "There is no resource with this resource Id"
+            })
+        }
+    })
+    .catch(error => {
+        res.status(400).json({ 
+            message: `There was an error retrieving the resource with this resource Id: ${error.message}`
+        })
+    });
+}
+
 router.get("/", (req, res) => {
   resources
     .getResource()
@@ -16,6 +36,14 @@ router.get("/", (req, res) => {
         .json({ error: "Could not retrieve resources from the Server" });
     });
 });
+
+router.get('/:id', validateResourceId, (req, res) => {
+    try {
+        res.status(200).json(req.resourceItem);
+    }catch(error){
+        res.status(500).json({message: `There was an error retrieving this resource`});
+    }
+})
 
 router.post("/", (req, res) => {
   const resourceBody = req.body;
